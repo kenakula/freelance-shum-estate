d9d972c9fede6e69298c8264d3a1a89790df20c0
AgaevKen <AgaevK@qmobile.ru>
2022-08-19 08:10:33 +0300

    adds estate-info

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,4 @@
 import { initTogglers } from '../components/toggler/toggler';
-import { initLightgalleries } from './modules/init-lightgallery';
 import { initPage } from './modules/init-page';
 import { initSelects } from './modules/init-selects';
 import { initSliders } from './modules/init-sliders';
@@ -11,5 +10,4 @@ document.addEventListener('DOMContentLoaded', () => {
   initTogglers();
   initSliders();
   initSelects();
-  initLightgalleries();
 });
